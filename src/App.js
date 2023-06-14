f23e796c95bc5a30e61cad15a0521d69313de6bf
roodhouse <rughjm@gmail.com>
2023-06-14 12:02:37 -0500

    updates to dt

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Summary from './components/Summary';
 function App() {
   return (
     <div className="App flex flex-col justify-center items-center w-full md:flex-row md:h-screen">
-     <Score />
-     <Summary />
+      <div id='appWrapper' className='bg-white flex flex-col justify-center items-center w-full md:flex-row md:h-auto md:max-w-[768px] md:justify-between md:rounded-[32px] md:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]'>
+      <Score />
+      <Summary />
+      </div>
     </div>
   );
 }
